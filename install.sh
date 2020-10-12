d9401cebfcc39e5a7fbe85c278bd660614f1a0b6
systemctl603 <arjunsenthilvel@gmail.com>
2020-10-12 21:47:18 +0530

    Modified install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+curl -L https://get.oh-my.fish | fish
 
 ln -s ${BASEDIR}/emacs.d ~/.emacs.d
-ln -s ${BASEDIR}/zshrc ~/.zshrc
 ln -s ${BASEDIR}/omf ~/.config/omf
