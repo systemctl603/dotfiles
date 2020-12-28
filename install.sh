4d46aefc3c852e7dbe4043012fdd5103e1635072
systemctl603 <arjunsenthilvel1@gmail.com>
2020-12-28 16:57:24 +0000

    Switch to arch

--- install.sh
+++ install.sh
@@ -6,16 +6,15 @@ then
     git clone https://github.com/systemctl603/dotfiles.git ~/.dotfiles
 fi
 
-while true
-do
-    echo "This will overwrite files in ~. Are you sure you want to do this (Y/n)? "	 
-    read stowOrNot
-    case $stowOrNot in
-				Y | y ) break ;;
-				n ) exit ;;
-				* ) ;;
-    esac
-done
+
+
+echo "This will overwrite files in ~. Are you sure you want to do this (Y/n)? "	 
+read stowOrNot
+case $stowOrNot in
+	Y | y ) ;;
+	n | *) exit ;;
+esac
+
 
 
 echo "Symlinking dotfiles"
@@ -26,3 +25,14 @@ stow -t ~ nixpkgs
 stow -t ~ sxhkd
 stow -t ~ bspwm
 
+echo "Installing fonts"
+stow -t ~ fonts
+
+echo "Would you like to install nixos configuration.nix too?"
+read nixosConf
+case $nixosConf in
+    Y | y ) ;;
+    n | *) exit ;;
+esac
+
+sudo stow -t / nixos
--- nixpkgs/.config/nixpkgs/home.nix
+++ nixpkgs/.config/nixpkgs/home.nix
@@ -5,4 +5,7 @@
     enable = true;
     package = pkgs.neovim-nightly;
   };
+  programs.alacritty = {
+    enable = true;
+  };
 }
